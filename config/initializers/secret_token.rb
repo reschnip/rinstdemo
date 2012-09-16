a9686d8c2cb490e7947f1b5d29d7c845b53300c6
Manuel Haas <finaleweisheit@gmail.com>
2012-09-16 13:34:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rinstdemo::Application.config.secret_token = 'b4b0759e22c1ac1ceb879d72d4c9bc93b3f6e64cea2490a800a37fc94a70b21bef5a7bb443f6f89d4077d07982f407924f1868aa3d1be62aa3e53c0bce01e994'
